936afdef25955dc861756bb1bb247ffdc9d5a12f
kgrac <kasandragraciano05@gmail.com>
2023-08-02 23:25:08 -0500

    Handles request of the form /hello?name-LaunchCode

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //every controller class we make needs a controller annotation
@@ -12,15 +13,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-    @GetMapping("hello") //handles request at path /hello
-    @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
-    @GetMapping("goodbye") //handles request at path /goodbye
+//    @GetMapping("hello") //handles request at path /hello
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+    @GetMapping("goodbye") //handles request at path /goodbye and calls the method goodbye()
     @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
+        //Get request are used only to request data example localhost8080:/goodbye calls the goodbye
+        //controller method and returns "Goodbye, Spring!"
+    }
+
+
+    //Query parameters
+
+    // Handles request of the form /hello?name-LaunchCode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
     }
 
 }
