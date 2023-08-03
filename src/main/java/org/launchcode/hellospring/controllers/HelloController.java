088a79ff1384a7730cdc49687616087822c337db
kgrac <kasandragraciano05@gmail.com>
2023-08-03 12:19:16 -0500

    added notes to the query param method example

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -26,13 +26,18 @@ public class HelloController {
         //controller method and returns "Goodbye, Spring!"
     }
 
-
     //Query parameters
-
-    // Handles request of the form /hello?name-LaunchCode
+    // Handles request of the form /hello?name=LaunchCode
+    //Path parameters video
+    //Some controllers can take in parameters in the form of query strings.
+    //query strings are URL data, the key-value pairs
+    //@RequestParam annotation notifies spring that the name parameter is going to be in the query parameter(end of URL)
+    //a dynamic response will take a piece of data from the request and give a different response based on the value of that data. the example below shows us one way.
+    //cannot have 2 different controllers at the same path. first method in this class was commented for the method below to run locally.
     @GetMapping("hello")
     @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
+
         return "Hello, " + name + "!";
     }
 
