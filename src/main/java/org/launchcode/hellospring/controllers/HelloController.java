01302b508f816efae3dd65d4a2d51a62b8f35718
kgrac <kasandragraciano05@gmail.com>
2023-08-09 15:17:17 -0500

    added a new method to determine which greeting to provide after form is submitted. updated form to show options

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
-@RequestMapping("/hello")
+@RequestMapping("hello")
 public class HelloController {
 
     //    @GetMapping("hello") //handles request at path /hello
@@ -17,47 +17,67 @@ public class HelloController {
 //    }
 
     //with @RequestMapping("hello") being at class level, the path will always begin with /hello
-    //This now lives /hello/goodbye since we use @GetMapping("goodbye") on the method itself
 
+    //This now lives /hello/goodbye since we use @GetMapping("goodbye") on the method itself
     @GetMapping("goodbye") //handles request at path /goodbye and calls the method goodbye()
     public String goodbye() {
         return "Goodbye, Spring!";
-
     }
 
-
-    //this method now lives at /hello since we have to value inside of @RequestMapping
+    //Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name, @RequestParam String friend) {
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
 
-        return "Hello, my name is " + name + ", and this is my friend " + friend + "!";
+    //Handles requests of the form /hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @PostMapping("message")
+    public String createMessage(@RequestParam String name, @RequestParam String languages) {
+        String greeting = "";
+        if (languages.equals("english")) {
+            greeting = "Hello, " + name + "!";
+
+        } else if (languages.equals("spanish")) {
+            greeting = "Hola, " + name + "!";
+        } else if (languages.equals("german")) {
+            greeting = "Hallo, " + name + "!";
+        } else if (languages.equals("czech")) {
+            greeting = "Ahoj, " + name + "!";
+        } else if (languages.equals("portuguese")) {
+            greeting = "Olá, " + name + "!";
+        }
+
+        return greeting;
     }
 
 
-//    @GetMapping("{name}")
-//    public String hellowWithPathParam(@PathVariable String name) {
-//        return "Hello, " + name +"!";
-//    }
 
-
-
-    //lives at /hello/form
-
-    //Form submission
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action='/hello'>" + //submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='text' name='friend'>" +
-                "<input type='submit' value='Greet Me!'>" +
+                "<form action = '/hello/message' method = 'post'>" +
+                "<input type = 'text' name = 'name' > " +
+                "<select name = 'languages' id = 'lang-select'>" +
+                "<option value = 'english'> English </option>" +
+                "<option value = 'spanish'> Spanish </option>" +
+                "<option value = 'german'> German </option>" +
+                "<option value = 'czech'> Czech </option>" +
+                "<option value = 'portuguese'> Portuguese </option>" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
-
-
     }
+
+
+
+
 
 
     }
