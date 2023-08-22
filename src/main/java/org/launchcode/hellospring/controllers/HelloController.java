c463c19316ae84a3b17124ff91e479f870021d2d
kgrac <kasandragraciano05@gmail.com>
2023-08-22 15:38:38 -0500

    created templates to interact with controller dynamically

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //every controller class we make needs a controller annotation
 
@@ -26,27 +30,49 @@ public class HelloController {
 
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
 
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String hellowWithPathParam(@PathVariable String name) {
-        return "Hello, " + name +"!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
         //Form submission
         @GetMapping("form")
         public String helloForm() {
         return "form";
+    }
+//dynamic views is when contents are changed when data is entered per requests
 
 
-        }
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
 
+
+        return "helloList";
+    }
+
+    @GetMapping("coffee-list")
+    public String coffeeNames(Model model) {
+        List<String> coffeeOptions = new ArrayList<>();
+        coffeeOptions.add("Black coffee");
+        coffeeOptions.add("frappe");
+        coffeeOptions.add("Iced coffee");
+        model.addAttribute("coffeeOptions", coffeeOptions);
+        return "coffeeList";
+    }
 
     }
 
